d2bcabc5583d0e0250f2d5d98d0dc08a6c56254a
ivan <ivan.torres@rocketfy.es>
2021-04-26 22:37:08 +0200

    Fix commit

---
+++ database/migrations/2021_04_24_165830_create_friends_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->integer('friend_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('set null')->onDelete('set null');
+            $table->foreign('friend_id')->references('id')->on('users')->onUpdate('set null')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
---
+++ database/migrations/2021_04_24_170154_create_badges_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBadgesUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('badges_users', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->integer('badge_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null')->onUpdate('set null');
+            $table->foreign('badge_id')->references('id')->on('badges')->onDelete('set null')->onUpdate('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('badges_users');
+    }
+}

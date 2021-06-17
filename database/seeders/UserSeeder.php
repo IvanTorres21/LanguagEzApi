ec3ecb183c534f1dd92c7909581a7cef73914489
ivan <ivan.torres@rocketfy.es>
2021-06-17 06:06:05 +0200

    bla

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,6 +20,9 @@ class UserSeeder extends Seeder
             'name' => 'Admin',
             'email' => 'ivantorresoses@gmail.com',
             'password' => Hash::make('password'),
+            'points' => 0,
+            'level' => 0,
+            'admin' => true
         ]);
     }
 }
